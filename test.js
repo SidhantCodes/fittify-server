635a5be66efba0002707b92910a2b98ab50eefd2
Sidhant Mishra <sidhant.mishra2022@vitstudent.ac.in>
2024-10-05 11:55:38 +0530

    final commit

---
+++ test.js
@@ -0,0 +1,7 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Log the MongoDB URI
+console.log('MongoDB URI:', process.env.MONGODB_URI);
+
+// You can add more code here to test other environment variables if needed
